7a53f6f3a84a43faac927de8eb77b6c010dfb3d7
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-06-15 13:20:43 +0530

    wallet connect added - with all dep,

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 import { Layout, Row, Col } from "antd";
+
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 
 function App() {
   return (
     <>
-    <Layout>
-      <Row align="middle">
-        <Col span={10} offset={2}>
-          <h1>Our todolist</h1>
-        </Col>
-        <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-          <h1>Connect Wallet</h1>
-        </Col>
-      </Row>
-    </Layout>
-  </>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <WalletSelector />
+          </Col>
+        </Row>
+      </Layout>
+    </>
   );
 }
 
