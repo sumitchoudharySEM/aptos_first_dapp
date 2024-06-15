4e5df6d26873e558af6effa0e59c31900e14fc43
sumitchoudharySEM <sumitsem2004@gmail.com>
2024-06-15 16:43:25 +0530

    submit create new list transaction to chaain done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,67 @@
 import React from "react";
-import { Layout, Row, Col, Button } from "antd";
+import { Layout, Row, Col, Button, Spin } from "antd";
 import { useEffect, useState } from "react";
 
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import { Aptos } from "@aptos-labs/ts-sdk";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useWallet, InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 
 const aptos = new Aptos();
 
-function App() {
+type Task = {
+  address: string;
+  completed: boolean;
+  content: string;
+  task_id: string;
+};
 
-  const { account } = useWallet();
+
+function App() {
+  const { account, signAndSubmitTransaction } = useWallet();
+  const moduleAddress =
+  '0xb50f46b6344af99dd208f35e780b7803ccbfb4a626389e139f2215afb70a1e56';
 
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
+  const [transactionInProgress, setTransactionInProgress] =
+  useState<boolean>(false);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  
 
   const fetchList = async () => {
     if (!account) return [];
-    // change this to be your module account address
-    const moduleAddress = "b50f46b6344af99dd208f35e780b7803ccbfb4a626389e139f2215afb70a1e56";
     try {
-      const todoListResource = await aptos.getAccountResource(
-        {
-          accountAddress:account?.address,
-          resourceType:`${moduleAddress}::todolist::TodoList`
-        }
-      );
+      const todoListResource = await aptos.getAccountResource({
+        accountAddress: account?.address,
+        resourceType: `${moduleAddress}::todolist::TodoList`,
+      });
+      console.log(todoListResource, "todoListResource is now true");
       setAccountHasList(true);
     } catch (e: any) {
+      console.log(e, "todoListResource is now false");
       setAccountHasList(false);
+    }
+  };
+
+  const addNewList = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    const transaction:InputTransactionData = {
+        data: {
+          function:`${moduleAddress}::todolist::create_list`,
+          functionArguments:[]
+        }
+      }
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(transaction);
+      // wait for transaction
+      await aptos.waitForTransaction({transactionHash:response.hash});
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
     }
   };
 
@@ -38,30 +71,33 @@ function App() {
 
   return (
     <>
-    <Layout>
-      <Row align="middle">
-        <Col span={10} offset={2}>
-          <h1>Our todolist</h1>
-        </Col>
-        <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-          <WalletSelector />
-        </Col>
-      </Row>
-    </Layout>
-    {!accountHasList && (
-      <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-        <Col span={8} offset={8}>
-          <Button
-            block
-            type="primary"
-            style={{ height: "40px", backgroundColor: "#3f67ff" }}
-          >
-            Add new list
-          </Button>
-        </Col>
-      </Row>
-    )}
-  </>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <WalletSelector />
+          </Col>
+        </Row>
+      </Layout>
+      <Spin spinning={transactionInProgress}>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button
+              onClick={addNewList}
+              block
+              type="primary"
+              style={{ height: "40px", backgroundColor: "#3f67ff" }}
+            >
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
+    </Spin>
+    </>
   );
 }
 
